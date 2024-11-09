9b75a7319ac1c31b9fa3f4a27b3b73262739885f
Jacek Szulc <j.szulc77@wp.pl>
2024-11-09 19:42:47 +0100

    APP.2

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <?php
     $title = 'APP';
 
+    session_start();
     $login = "janek";
     $pass = "1234";
     if(isset($_POST["submit"])) {
-      if(isset($_POST["login"]) && isset($_POST["password"])) {
-        if($login == $_POST["login"] && $pass == $_POST["password"]) {
-          echo "Logowanie z sukcesem !!!";
-        }else{
-          echo "BŁĘDNY LOGIN LUB HASŁO";
+        if(isset($_POST["login"]) && isset($_POST["password"])) {
+            if($login == $_POST["login"] && $pass == $_POST["password"]) {
+                echo "Logowanie z sukcesem !!!";
+                echo session_id();
+                $_SESSION["loginUser"] = session_id();
+            }else{
+                echo "BŁĘDNY LOGIN LUB HASŁO";
+            }
         }
-      }
-      echo $_POST["login"] . " " . $_POST["password"];
+        echo $_POST["login"] . " " . $_POST["password"];
+    }
+
+    if(isset($_POST["logout"])) {
+        session_destroy();
+        header("Location: index.php");
     }
 ?>
 
@@ -29,20 +37,30 @@
     <!-- <h1><?php echo $title; ?></h1> -->
 
     <div class="app-login-start">
-
-        <form action="index.php" method="post">
-            <div class="mb-3">
-              <label for="login" class="form-label">Login</label>
-              <input type="text" class="form-control" id="login" name="login">
-            </div>
-            <div class="mb-3">
-              <label for="password" class="form-label">Password</label>
-              <input type="password" class="form-control" id="password" name="password">
-            </div>
-            <button type="submit" class="btn btn-primary" name="submit">Login</button>
-            <button type="reset" class="btn btn-secondary">Cancel</button>
-        </form>
-
+        <?php
+            if(isset($_SESSION["loginUser"])) {
+        ?>
+            <form action="index.php" method="post">
+                <button type="submit" class="btn btn-success" name="logout">Log out</button> 
+            </form>
+        <?php    
+            } else {
+        ?>
+            <form action="index.php" method="post">
+                <div class="mb-3">
+                    <label for="login" class="form-label">Login</label>
+                    <input type="text" class="form-control" id="login" name="login">
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" name="password">
+                </div>
+                <button type="submit" class="btn btn-primary" name="submit">Login</button>
+                <button type="reset" class="btn btn-secondary">Cancel</button>
+            </form>
+        <?php
+            }     
+        ?>
     </div><!--end-app-login-start-->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
